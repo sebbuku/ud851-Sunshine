378d0acb7fd83234192926cb48f79ba072420456
Sebastian Bartus-Kunz <sbartus-kunz@nitton.de>
2018-09-16 20:14:29 +0200

    Completed S04.03

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,8 +2,14 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import static android.support.v4.app.ShareCompat.*;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -29,6 +35,31 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
+    // COMPLETED (3) Create a menu with an item with id of action_share
     // TODO (4) Display the menu and implement the forecast sharing functionality
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.detail, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            ShareCompat.IntentBuilder intentBuilder = ShareCompat.IntentBuilder
+            .from(this)
+            .setType("text/plain")
+            .setChooserTitle("Choose an app to share the weather data")
+            .setText(mForecast);
+            Intent shareIntent = intentBuilder.getIntent();
+
+            startActivity(shareIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
